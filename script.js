f4b97347ab79cad46cee05dd3764c3fde1a7ec49
Daniel-Nlemedim <nzubenlems2002@gmail.com>
2024-06-13 08:13:11 +0100

    added html file, and some pictures

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-'use strict'
+"use strict";
+
+//prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const containerWorkout = document.querySelector(".workout");
+const form = document.querySelector(".form");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputType = document.querySelector(".form__input--type");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
+const inputDuration = document.querySelector(".form__input--duration");
+const formBtn = document.querySelector(".form__btn");
